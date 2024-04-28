71aa7df9e34c1fabf1a76a9076501c488f65fa11
falling-ts <zgh.yuanshang@gmail.com>
2024-04-28 11:04:44 +0800

    feat 添加用户端

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from webui_pages.utils import *
+from client_pages.utils import *
 from streamlit_option_menu import option_menu
-from webui_pages.dialogue.dialogue import dialogue_page, chat_box
-from webui_pages.knowledge_base.knowledge_base import knowledge_base_page
+from client_pages.dialogue.dialogue import dialogue_page, chat_box
+from client_pages.knowledge_base.knowledge_base import knowledge_base_page
 import os
 import sys
 from configs import VERSION
@@ -15,11 +15,14 @@ if __name__ == "__main__":
     is_lite = "lite" in sys.argv
 
     st.set_page_config(
-        "人工智能",
+        "日照城投智能聊天系统",
         os.path.join("img", "chatchat_icon_blue_square_v2.png"),
-        layout="wide",
         initial_sidebar_state="expanded",
-        menu_items=None
+        menu_items={
+            'Get Help': 'https://github.com/chatchat-space/Langchain-Chatchat',
+            'Report a bug': "https://github.com/chatchat-space/Langchain-Chatchat/issues",
+            'About': f"""欢迎使用 Langchain-Chatchat WebUI {VERSION}！"""
+        }
     )
 
     pages = {
@@ -33,16 +36,29 @@ if __name__ == "__main__":
         },
     }
 
-    tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+    with st.sidebar:
+        st.image(
+            os.path.join(
+                "img",
+                "logo-long-chatchat-trans-v2.png"
+            ),
+            use_column_width=True
+        )
+        st.caption(
+            f"""<p align="right">当前版本：{VERSION}</p>""",
+            unsafe_allow_html=True,
+        )
+        options = list(pages)
+        icons = [x["icon"] for x in pages.values()]
 
-    with tab1:
-        st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+        default_index = 0
+        selected_page = option_menu(
+            "",
+            options=options,
+            icons=icons,
+            # menu_icon="chat-quote",
+            default_index=default_index,
+        )
 
-    with tab2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-    with tab3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    if selected_page in pages:
+        pages[selected_page]["func"](api=api, is_lite=is_lite)
---
+++ client_pages/model_config/model_config.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from client_pages.utils import *
+
+def model_config_page(api: ApiRequest):
+    pass
